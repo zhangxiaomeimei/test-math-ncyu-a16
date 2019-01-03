e8530750354bd7295d2aba668d02a9906535b653
etrex kuo <kamigo.service@gmail.com>
2019-01-03 14:42:14 +0800

    second ver

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
   <h2>Basic Table</h2>
   <p>The .table class adds basic styling (light padding and horizontal dividers) to a table:</p>
 <a href="test.html">test.html</a>
+<a href="test.html">test.html</a>
   <table class="table">
     <thead>
       <tr>
